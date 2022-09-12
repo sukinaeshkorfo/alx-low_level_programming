e9d3669258710e19d6cb7d96e71b368a7c7ee93a
sukinaeshkorfo <sukina1998eshkorfo@gmail.com>
2022-09-12 16:36:45 -0700

    task 0123

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+
+}
