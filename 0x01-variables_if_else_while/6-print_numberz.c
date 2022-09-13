3143b840c99433a63be45a8ae4cc3f9336bf4d3f
sukinaeshkorfo <sukina1998eshkorfo@gmail.com>
2022-09-12 17:53:10 -0700

    all tasks

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0-9 without using char or printf/puts,
+ * and putchar 2x max
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int s = '0';
+
+	while (s <= '9')
+	{
+		putchar(i);
+		s++;
+	}
+	putchar('\n');
+	return (0);
+}
