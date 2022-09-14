93337ea1e3bbad0132e1e8720849ea8cd0c25236
sukinaeshkorfo <sukina1998eshkorfo@gmail.com>
2022-09-13 19:39:35 -0700

    addation

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit for a given number
+ *
+ * @x: first integer num 
+ * @y:second integer num
+ *
+ * Return: int 
+ */
+int add(int x , int y)
+
+{
+      return (x+y);
+}
+
