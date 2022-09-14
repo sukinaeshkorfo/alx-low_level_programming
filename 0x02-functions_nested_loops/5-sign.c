629515f67a87df0e13161a7f8663aa6786ec78d5
sukinaeshkorfo <sukina1998eshkorfo@gmail.com>
2022-09-13 17:59:20 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,18 @@
 #include "main.h"
-#include <ctype.h>
 /**
- * print_sign - print sign of a number
- *
- * Return: int 1 if greater than 0, 0 if 0, -1 if less than 0
+ * _abs - calculate the absolute value of the number from zero.
+ * @i: the integer to be converted.
+ * Return: absolute value of the input number.
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		i = i * -1;
+	return (i);
+}/**
+ * print_sign - Determines whether an integer is positive, negative or zero.
+ * @i: the number to be checked.
+ * Return: 1 if positive, 0 if zero, -1 if negative, / if not a digit.
  */
 int print_sign(int i)
 {
@@ -12,11 +21,19 @@ int print_sign(int i)
 		_putchar('+');
 		return (1);
 	}
+	else if (i == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
 	else if (i < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
-	_putchar('0');
-	return (0);
-}
+	else
+	{
+		_putchar('-');
+		return ('/');
+	}
+
