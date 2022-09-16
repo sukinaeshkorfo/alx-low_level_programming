93b5d4a35bfaed66578cece652ad3999ff78951d
sukinaeshkorfo <sukina1998eshkorfo@gmail.com>
2022-09-16 15:11:45 -0700

    issupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * _isupper - check the code
+ * @c: blablalba
+ * Return: void
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _islower - check Lower Case Char
+ * _putchar - prototype.
+ * print_alphabet - print a..z
+ * print_alphabet_x10 - print a..z 10 times
+ * Return: nothing.
+ */
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
